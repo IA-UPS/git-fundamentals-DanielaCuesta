049514add01295e1b5d540d64894f49a3ae5cac6
danielaacuestas <dcuestas1@est.ups.edu.ec>
2023-04-02 18:37:28 -0500

    Clouds of Points

--- IntroduccionRStudio.R
+++ IntroduccionRStudio.R
@@ -103,3 +103,33 @@ ggplot()+
   geom_line(data=line,aes(x=x,y=y))+
   geom_point(data=means,aes(x=x,y=y),size=5,color="red")+
   geom_point(data=dat,aes(x=x,y=y))
+x<-c(2,4,9,15)
+sqrt(x)
+x^2
+sapply(x,function(x) sqrt(x))
+sapply(x,function(x) rnorm(1,x,10))
+x<-rnorm(100,10,5)
+y<-3*x+1       
+means<-data.frame(x,y)
+means
+ggplot()+
+  geom_point(data=means,aes(x=x,y=y),size=5,color="red")
+x<-c(-5,25)
+y<-3*x+1
+line<-data.frame(x,y)
+line
+ggplot()+
+  geom_point(data=means,aes(x=x,y=y),color="red")+
+  geom_line(data=line,aes(x=x,y=y))+
+  scale_x_continuous(limits = c(-10,30))+
+  scale_y_continuous(limits = c(-20,80))
+means
+x<-means$x
+y<-sapply(x,function(x) rnorm(1,3*x+1))
+dat<-data.frame(x,y)
+ggplot()+
+  geom_point(data=means,aes(x=x,y=y),color="red")+
+  geom_line(data=line,aes(x=x,y=y))+
+  scale_x_continuous(limits = c(-10,30))+
+  scale_y_continuous(limits = c(-20,80))+
+  geom_point(data=dat,aes(x=x,y=y))
